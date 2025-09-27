77c66e3308fc9852ff94262dced9d01b19202ebb
Nguyen Van Tuyen <tuyennv216@outlook.com>
2025-09-28 02:39:41 +0700

    update code snippet view height

---
+++ PTMngVSIX/ToolWindow/Converter/MathMultiplyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PTMngVSIX.ToolWindow.Converter
+{
+	internal class MathMultiplyConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (values.Length >= 2 && values[0] is double value1 && values[1] is double value2)
+			{
+				return value1 * value2;
+			}
+			return DependencyProperty.UnsetValue;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
